ef0ee03eb3fc42bd088e3bbf9d74011818e9998f
hakurouken <ticshot@gmail.com>
2016-09-01 23:52:43 +0800

    1kkk searcher

---
+++ src/crawler/1kkk/search.js
@@ -0,0 +1,28 @@
+import { searchFuncGenerator } from 'lib/crawler/search-util';
+import cheerio from 'cheerio';
+
+const URL = 'http://www.1kkk.com/search?title={{keyword}}';
+const ID_MATCHER = /manhua(\d+)\//;
+
+const search = searchFuncGenerator(URL,function(body){
+    let $ = cheerio.load(body);
+    let DESC_START = /^简介：/;
+    return $('.main .kk2 ul').toArray().slice(1).map(elem => {
+        let $elem = $(elem);
+        let $cell = $elem.find('li');
+        let $link = $elem.find('a.a_comicname');
+        let id = ($link.attr('href').match(ID_MATCHER) || [])[1];
+        id = +id || null;
+        let $tk = $('#tk_'+id);
+
+        return {
+            id: id,
+            name: $link.text().trim(),
+            description: $tk.find('.tk_04').text().trim().replace(DESC_START,'').replace('\r\n',''),
+            author: $cell.eq(1).text().trim(),
+            cover: $elem.find('img.a_comicname').attr('src')
+        }
+    });
+});
+
+export { search };
