eb6ccd16e77d9a7cda3ea64c397dfda52b8ac4f9
hakurouken <ticshot@gmail.com>
2016-09-07 20:29:42 +0800

    link dist/crawler folder to node_modules/crawler

--- postinstall.js
+++ postinstall.js
@@ -1,12 +1,18 @@
 const fs = require('fs');
 const path = require('path');
-const src = path.join(__dirname,'dist/lib');
-const dst = path.join(__dirname,'node_modules/lib');
 
-fs.lstat(dst, (err,stat) => {
-    if (!stat) {
-        fs.symlink(src,dst,'dir',(err)=> {
-            if(err) throw err;
-        });
-    }
-});
+function linkToNodeModule(src,dst){
+    dst = path.join(__dirname,'node_modules',dst || src);
+    src = path.join(__dirname,'dist',src);
+
+    fs.lstat(dst, (err,stat) => {
+        if (!stat) {
+            fs.symlink(src,dst,'dir',(err)=> {
+                if(err) throw err;
+            });
+        }
+    });
+}
+
+linkToNodeModule('lib');
+linkToNodeModule('crawler');
