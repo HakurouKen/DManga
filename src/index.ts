69c665a84118bb90bac22eda8425635a6ceacafb
hakurouken <ticshot@gmail.com>
2018-08-26 16:11:05 +0800

    Remove test files.

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-
-import DmzjManga from './crawlers/manga/dmzj';
-
-const manga = new DmzjManga('https://manhua.dmzj.com/yiquanchaoren');
-
-manga.getInfo().then(console.log).catch((e) => { console.log(e); });
