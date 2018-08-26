ee7a217a3b195d8f4b2cfbd36cf41f11abce4be9
hakurouken <ticshot@gmail.com>
2018-08-26 09:56:23 +0800

    Add comments.

--- src/utils/types.d.ts
+++ src/utils/types.d.ts
@@ -1,3 +1,6 @@
+// Due to the bug of typescript-eslint-parser, the key `name` must be wrapped by quotes
+// See more at https://github.com/eslint/typescript-eslint-parser/issues/414
+
 export interface ChapterInfo {
   'name': string,
   url: string
