5ed945c6e966df7689db6e4f9a28831a4700b14b
hakurouken <ticshot@gmail.com>
2018-08-28 21:39:30 +0800

    Add crawlers/dm5/chapter.

---
+++ src/crawlers/dm5/chapter.ts
@@ -0,0 +1,121 @@
+import url from 'url';
+import cheerio from 'cheerio';
+import superagent from 'superagent';
+import { fetchText, batchDownload, fetchDocument } from '../../utils/request';
+import { exec } from '../../utils/misc';
+
+interface metaData {
+  cid: string;
+  key: string;
+  language: 1;
+  gtk: 6;
+  _cid: string;
+  _mid: number;
+  _dt: string;
+  _sign: string;
+}
+
+interface pageMetaData extends metaData {
+  total: number;
+}
+
+function getMeta(source: string, key: string): any {
+  const partsRegex = new RegExp(`${key}\\s*=\\s*(.*?);`);
+  const matched = source.match(partsRegex);
+  if (!matched) {
+    return null;
+  }
+  const variable = matched[1].trim();
+  // string-liked
+  if (variable[0] === variable[variable.length - 1] && ["'", '"'].indexOf(variable[0]) >= 0) {
+    return variable.slice(1, -1);
+  }
+  // number-liked
+  if (/^[0-9]+$/.test(variable)) {
+    return parseInt(variable, 10);
+  }
+  // do not support other situations
+  return null;
+}
+
+export default class Dm5Chapter {
+  url: string;
+
+  private $doc: Promise<CheerioStatic> | undefined;
+
+  private cookies: string = '';
+
+  constructor(pageUrl: string) {
+    this.url = pageUrl;
+  }
+
+  private $() {
+    if (!this.$doc) {
+      this.$doc = fetchDocument(this.url);
+    }
+    return this.$doc;
+  }
+
+  async getMetaData(): Promise<pageMetaData> {
+    const $ = await this.$();
+    const scripts = $('head script')
+      .not('[src]')
+      .toArray();
+
+    let metaDataSource = '';
+    for (let i = 0; i < scripts.length; i++) {
+      const source = cheerio(scripts[i]).html() || '';
+      if (source.indexOf('DM5_VIEWSIGN') >= 0) {
+        metaDataSource = source;
+        break;
+      }
+    }
+
+    const cid = getMeta(metaDataSource, 'DM5_CID');
+    return {
+      cid,
+      key: '',
+      language: 1,
+      gtk: 6,
+      _cid: cid,
+      _mid: getMeta(metaDataSource, 'DM5_MID'),
+      _dt: getMeta(metaDataSource, 'DM5_VIEWSIGN_DT'),
+      _sign: getMeta(metaDataSource, 'DM5_VIEWSIGN'),
+      total: getMeta(metaDataSource, 'DM5_IMAGE_COUNT'),
+    };
+  }
+
+  async getUrls() {
+    const { total, ...requestMeta } = await this.getMetaData();
+    const baseUrl = url.resolve(this.url, 'chapterfun.ashx');
+    let urls: string[] = [];
+    while (urls.length < total) {
+      const search = new url.URLSearchParams(
+        Object.assign(requestMeta, { page: (urls.length + 1).toString() }),
+      );
+      const responseText = await fetchText(`${baseUrl}?${search.toString()}`, {
+        referer: this.url,
+      });
+      try {
+        const nextUrls = exec(`${responseText};return d;`);
+        if (!nextUrls.length) {
+          throw new Error();
+        }
+        urls = urls.concat(nextUrls);
+      } catch (e) {
+        // console.error(e);
+        return urls;
+      }
+    }
+    return urls;
+  }
+
+  async download(dest: string) {
+    const urls = await this.getUrls();
+    return batchDownload(urls, dest, {
+      headers: {
+        referer: this.url,
+      },
+    });
+  }
+}
