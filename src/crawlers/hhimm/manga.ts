7e6463e990fe216eaf7b284e9f170a8d6fb2c00c
hakurouken <ticshot@gmail.com>
2018-09-03 23:41:47 +0800

    Add manga/hhimm.

---
+++ src/crawlers/hhimm/manga.ts
@@ -0,0 +1,54 @@
+import BaseManga from '../../base/manga';
+import { getChapterInfoFromAnchor } from '../../utils/misc';
+import { ChapterInfo } from '../../utils/types';
+
+function getChapters(
+  $chapters: Cheerio,
+  $titles: Cheerio,
+  index: number = 0,
+): { name: string; chapters: ChapterInfo[] } {
+  return {
+    name: $titles
+      .eq(index)
+      .text()
+      .trim(),
+    chapters: $chapters
+      .eq(index)
+      .find('li a')
+      .toArray()
+      .map(el => getChapterInfoFromAnchor(el, 'http://www.hhimm.com/')),
+  };
+}
+
+export default class HhimmManga extends BaseManga {
+  async getInfo() {
+    const $ = await this.$();
+    const $infos = $('#about_kit li');
+    const $titles = $('.cVolTag');
+    const $chapters = $('.cVolUl');
+
+    return {
+      name: $infos
+        .eq(0)
+        .find('h1')
+        .text()
+        .trim(),
+      url: this.url,
+      cover: $('#about_style img').attr('src'),
+      authors: [
+        $infos
+          .eq(1)
+          .text()
+          .replace(/^\s*作者:\s*|\s+$/g, ''),
+      ],
+      end: /完/.test($infos.eq(2).text()),
+      detail: $infos
+        .last()
+        .text()
+        .replace(/简介\s+:/, ''),
+      chapters: getChapters($chapters, $titles, 0).chapters,
+      otherVersions: Array.from({ length: $titles.length - 1 })
+        .map((_, index) => getChapters($chapters, $titles, index + 1)),
+    };
+  }
+}
---
+++ test/hhimm/manga.spec.js
@@ -0,0 +1,25 @@
+import Manga from '../../lib/crawlers/hhimm/manga';
+
+describe('Hhimm', () => {
+  describe('Manga#getInfo', () => {
+    let info;
+    beforeEach(async () => {
+      const manga = new Manga('http://www.hhimm.com/manhua/28308.html');
+      info = await manga.getInfo();
+    });
+
+    it.only("contains manga's info", () => {
+      info.name.should.equal('姐妹家的种种事');
+      info.url.should.equal('http://www.hhimm.com/manhua/28308.html');
+      info.cover.should.equal('http://img.94201314.net/comicui/28308.JPG');
+      info.authors.should.deep.equal(['高野うい']);
+      info.end.should.be.true();
+      info.detail.should.not.be.empty();
+      info.chapters.length.should.equal(1);
+      info.otherVersions.length.should.equal(1);
+      const otherVersion = info.otherVersions[0];
+      otherVersion.name.should.not.be.empty();
+      otherVersion.chapters.length.should.gte(1);
+    });
+  });
+});
