b9b5b81b15d211ebc2ed715021613db17ab506c8
hakurouken <ticshot@gmail.com>
2018-09-09 17:07:05 +0800

    Add verydm/manga.

---
+++ src/crawlers/verydm/manga.ts
@@ -0,0 +1,47 @@
+import cheerio from 'cheerio';
+import BaseManga from '../../base/manga';
+import { getChapterInfoFromAnchor } from '../../utils/misc';
+import { ChapterInfo } from '../../utils/types';
+
+function getChapters(el: Cheerio | CheerioElement): ChapterInfo[] {
+  return cheerio(el)
+    .find('li a')
+    .toArray()
+    .map(anchor => getChapterInfoFromAnchor(anchor, 'http://www.verydm.com'));
+}
+
+export default class MangaVerydm extends BaseManga {
+  async getInfo() {
+    const $ = await this.$();
+    const $infos = $('.detail li');
+    const $chapterLists = $('.chapters ul');
+
+    return {
+      name: $('.comic-name h1')
+        .text()
+        .trim(),
+      url: this.url,
+      cover: $('.info img').attr('src'),
+      authors: $infos
+        .eq(0)
+        .find('a')
+        .toArray()
+        .map(el => cheerio(el).text()),
+      end: /完/.test($infos.eq(2).text()),
+      description: $('#content_wrapper')
+        .text()
+        .trim(),
+      chapters: getChapters($chapterLists.eq(0)),
+      otherVersions: $chapterLists
+        .slice(1)
+        .toArray()
+        .map((ul) => {
+          const $ul = cheerio(ul);
+          return {
+            name: $ul.prev().text(),
+            chapters: getChapters($ul),
+          };
+        }),
+    };
+  }
+}
---
+++ test/crawlers/verydm/manga.spec.js
@@ -0,0 +1,26 @@
+import Manga from '../../../lib/crawlers/verydm/manga';
+
+describe('Verydm', () => {
+  describe('Manga#getInfo', () => {
+    let info;
+    beforeEach(async () => {
+      const manga = new Manga('http://www.verydm.com/manhua/langyuxxl');
+      info = await manga.getInfo();
+    });
+
+    it("contains manga's info", () => {
+      info.name.should.equal('狼与香辛料');
+      info.url.should.equal('http://www.verydm.com/manhua/langyuxxl');
+      info.cover.should.match(/\.jpg$/);
+      info.authors.should.deep.equal(['小梅系斗']);
+      info.end.should.be.true();
+      info.description.should.not.be.empty();
+      info.chapters.length.should.not.equal(0);
+      info.otherVersions.length.should.not.equal(0);
+      info.otherVersions.forEach((version) => {
+        version.name.should.not.be.empty();
+        version.chapters.should.not.be.empty();
+      });
+    });
+  });
+});
