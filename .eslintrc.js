f159e79f3ea3a0388f6461ccf5cb03d81012af32
hakurouken <ticshot@gmail.com>
2018-09-22 16:36:46 +0800

    Allow for-of statement.

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,21 @@ module.exports = {
     "no-console": "off",
     "no-plusplus": "off",
     "no-underscore-dangle": ["error", { "allowAfterThis": true, "allowAfterSuper": true }],
+    "no-restricted-syntax": [
+      "error",
+      {
+        selector: 'ForInStatement',
+        message: 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+      },
+      {
+        selector: 'LabeledStatement',
+        message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+      },
+      {
+        selector: 'WithStatement',
+        message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+      }
+    ]
   },
   "overrides": [{
     "files": ["**/*.ts"],
@@ -31,6 +46,7 @@ module.exports = {
     "rules": {
       "import/no-unresolved": "off",
       "no-undef": "off",
+      "no-unused-vars": "off",
       "typescript/no-array-constructor": ["error"],
       "typescript/no-triple-slash-reference": ["error"],
       "typescript/no-unused-vars": ["error"]
